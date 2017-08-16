a8743a15b3d8b75f4399ce3f3dda2ed063a96887
DÍAZ-GAY <DIAZ2@csc.es>
2017-08-16 19:10:10 +0200

    First ideas

--- MutSignatures/ui.R
+++ MutSignatures/ui.R
@@ -1,33 +1,17 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Mutational Signatures"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       fileInput("fileinput",""),
+       radioButtons("datatype", "Formato", c("vcf"))
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("resultplot")
     )
   )
 ))
