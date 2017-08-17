a318ffc29879de8bdfa227dbcdea95c8584729ec
DÍAZ-GAY <m.diaz.gay@gmail.com>
2017-08-17 18:18:33 +0200

    Changes in ui & server

--- MutSignatures/server.R
+++ MutSignatures/server.R
@@ -1,14 +1,36 @@
 library(shiny)
 
 shinyServer(function(input, output) {
+
+   options(shiny.maxRequestSize=300*1024^2)
    
-  output$resultplot <- renderPlot({
+   
+   library(MutationalPatterns)
     
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   ref_genome<-reactive({
+      if (input$genome=="38") return ("BSgenome.Hsapiens.NCBI.GRCh38")
+      if (input$genome=="19") return ("BSgenome.Hsapiens.UCSC.hg19")
+      if (input$genome=="37") return ("BSgenome.Hsapiens.1000genomes.hs37d5")
+   })
+
+
+   
+
+   
+   
+   vcfs<-reactive({
+      library(ref_genome(), character.only = TRUE)
+      inFile<-input$fileinput
+      if (input$datatype=="vcf")
+         return(read_vcfs_as_granges(inFile$datapath,inFile$dataname,ref_genome))
+   })   
+   
+   
     
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$result <- renderPrint({
     
-  })
+      print(summary(vcfs))
+    
+   })
   
 })
--- MutSignatures/ui.R
+++ MutSignatures/ui.R
@@ -6,12 +6,14 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-       fileInput("fileinput",""),
-       radioButtons("datatype", "Formato", c("vcf"))
+       helpText("Upload your file:"),
+       fileInput("fileinput","", multiple=TRUE),
+       radioButtons("datatype", "Formato", c("vcf")),
+       selectInput("genome","Reference Genome",c("NCBI GRCh38"="38","UCSC hg19"="19","1000genomes hs37d5"="37"),selected="37")
     ),
     
     mainPanel(
-       plotOutput("resultplot")
+       verbatimTextOutput("result")
     )
   )
 ))
