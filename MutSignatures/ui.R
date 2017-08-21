f39cd51de37a1656a104fdac3a7dd8ae22e70173
mariavica <maria.vila.cs@gmail.com>
2017-08-21 13:35:45 +0200

    Output includes known signatures in cancers

--- MutSignatures/ui.R
+++ MutSignatures/ui.R
@@ -15,8 +15,9 @@ shinyUI(fluidPage(
       mainPanel(
          tabsetPanel(type="tabs",
             tabPanel("Mutational profile of provided sample/s", plotOutput("prof96")),
-            tabPanel("Cosmic mutational signatures contributions", dataTableOutput("contr"))
-         )
+            tabPanel("Cosmic mutational signatures contributions", dataTableOutput("contr")),
+            tabPanel("Comparison with other cancers", dataTableOutput("known"))
+            )
       )
    )
 ))
