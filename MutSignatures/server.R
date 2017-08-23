27f4c02669d873b52c575e5454b392950ec8daab
Marcos Díaz-Gay <m.diaz.gay@gmail.com>
2017-08-23 13:25:28 +0200

    busyIcon, format help, etc

--- MutSignatures/server.R
+++ MutSignatures/server.R
@@ -1,6 +1,14 @@
 library(shiny)
+library(shinyBS)
+library(shinysky)
+library(shinyjs)
 
 shinyServer(function(input, output) {
+   
+   #Hidding tabs of mainpanel (results)
+   observeEvent(input$run,{
+      shinyjs::show(id="mainpanel",anim=TRUE,animType="slide")
+   })
 
    #Changing maximum file size for uploading
    options(shiny.maxRequestSize=300*1024^2)
@@ -21,19 +29,47 @@ shinyServer(function(input, output) {
          library ("BSgenome.Hsapiens.1000genomes.hs37d5")
          return ("BSgenome.Hsapiens.1000genomes.hs37d5")
       }
+      if (input$genome=="hg38"){
+         library("BSgenome.Hsapiens.UCSC.hg38")
+         return("BSgenome.Hsapiens.UCSC.hg38")
+      }
    })
  
    #Reading vcf files as GRanges objects
    vcfs<-eventReactive(input$run,{
       inFile<-input$fileinput
-      if (input$datatype=="vcf")
-         return(read_vcfs_as_granges(inFile$datapath,inFile$name,ref_genome()))
+
+#      withProgress(message="Running",value=0,{
+      
+         if (input$datatype=="vcf") return(read_vcfs_as_granges(inFile$datapath,inFile$name,ref_genome(),group = "auto+sex", check_alleles = TRUE))
+         
+#         incProgress(0.5,detail="GRanges")
+         
+#      })
+      
    })
-   
-   mut_mat <- reactive({ mut_matrix(vcfs(),ref_genome()) })
-   
+      
+   mut_mat <- reactive({
+#      withProgress(message="Running",value=0.5, {
+         return(mut_matrix(vcfs(),ref_genome()))
+         
+#         incProgress(0.5,detail="mut_matrix")
+#      })
+         
+         
+   })
+      
+      
+      
    output$prof96 <- renderPlot({
-      plot_96_profile(mut_mat())
+#      withProgress(message="Running",value=0,{
+         
+#         incProgress(0.5,detail="a")
+         
+         plot_96_profile(mut_mat())
+         
+#         incProgress(0.5,detail="b")
+ #     })
    })
 
    
@@ -52,21 +88,21 @@ shinyServer(function(input, output) {
    divisionRel<-function(df){
       sum_df<-sapply(df,sum)
       for (i in 1:ncol(df)){
-         df[,i]<-round((df[,i]/sum_df[i])*100,2)
+         df[,i]<-round((df[,i]/sum_df[i]),3)
       }
       return(df)
    }
    
    
    output$contr <- renderDataTable({
-      data.frame(Signature = colnames(cancer_signatures), Proposed_Etiology = proposed_etiology, divisionRel(as.data.frame(fit_res()$contribution)))
-   },options = list(lengthChange=FALSE,pageLength=30, paging=FALSE,orderClassses=TRUE))
+      data.frame(Signature = 1:30, Proposed_Etiology = proposed_etiology, divisionRel(as.data.frame(fit_res()$contribution)))
+   },options = list(lengthChange=FALSE,pageLength=30, paging=FALSE))
 
    output$download_contr <- downloadHandler( filename="contr.csv", content=function (file){ write.table(x=data.frame(colnames(cancer_signatures), proposed_etiology, fit_res()$contribution), file=file, row.names=FALSE, sep="\t", quote=FALSE) })
    
    output$known<- renderDataTable({
-      data.frame(colnames(cancer_signatures), divisionRel(as.data.frame(fit_res()$contribution)), known_cancer_signatures[c(-31),] )
-      })
+      data.frame(Signature = 1:30, divisionRel(as.data.frame(fit_res()$contribution)), known_cancer_signatures[c(-31),] )
+      },options = list(lengthChange=FALSE,pageLength=30, paging=FALSE))
 
    output$download_known <- downloadHandler( filename="known.csv", content=function (file){ write.table(x=data.frame(colnames(cancer_signatures), fit_res()$contribution, known_cancer_signatures[c(-31),]), file=file, row.names=FALSE, sep="\t", quote=FALSE) })
    
--- MutSignatures/ui.R
+++ MutSignatures/ui.R
@@ -1,6 +1,11 @@
 library(shiny)
+library(shinyBS)
+library(shinysky)
+library(shinyjs)
 
 shinyUI(fluidPage(
+   
+   useShinyjs(),
   
    titlePanel("Mutational Signatures"),
   
@@ -9,23 +14,56 @@ shinyUI(fluidPage(
          fileInput("fileinput","Upload your file/s", multiple=TRUE),
          helpText("Multiple files uploading is allowed"),
          hr(),
-         radioButtons("datatype", "Formato", c("vcf")),
-         selectInput("genome","Reference Genome",c("NCBI GRCh38"="38","UCSC hg19"="19","1000genomes hs37d5"="37"),selected="37"),
+         radioButtons("datatype", "Format", c("vcf","tsv","Excel"),selected = "vcf",inline = TRUE),
          
+         #Help menu for format of input file
+         actionLink("helpformat","Help with format of input file"),
+         bsModal("modal","HELP: Input file format","helpformat",
+                 HTML(
+                     "<p>Format of the input file provided to this aplication should be one of the following options:
+                     </p>
+                     <p>
+                     <u>vcf</u>: Variant Call Format according to the specification provided <a href=https://samtools.github.io/hts-specs/VCFv4.2.pdf>here</a> (optimized for 4.2 version).
+                     </p>
+                     <p>
+                     <u>tsv</u>: Tab Separated Values file with four required fields. A header line is mandatory and the four headers must be provided as listed below:</p>
+                     <ol>
+                     <li>CHROM - Chromosome, ‘chr’ prefix is optional (e.g. ‘chr10’ and ’10’ are both valid).</li>
+                     <li>POS - Chomosomal coordinate of reference allele.</li>
+                     <li>REF - Reference allele at position given above.</li>
+                     <li>ALT - Observed alternate allele.</li>
+                     </ol>
+                     </p>
+                     <p>
+                     <u>Excel</u>: Excel file (xls and xlsx extensions are both valid) with the same structure of four required fields described above (CHROM, POS, REF and ALT).
+                     </p>
+                     
+                     ")
+         ),
+         
+         
+         hr(),
+         selectInput("genome","Reference Genome",c("NCBI GRCh38"="38","UCSC hg38"="hg38","UCSC hg19"="19","1000genomes hs37d5"="37"),selected="37"),
+         
+         busyIndicator("Calculation in progress",wait=1),
          actionButton("run","Run")
       ),
+      
+      #Hidding tabs of mainpanel (results)
+      hidden(
     
-      mainPanel(
-         tabsetPanel(type="tabs",
-            tabPanel("Mutational profile of provided sample/s", plotOutput("prof96")),
-            tabPanel("Cosmic mutational signatures contributions", downloadButton("download_contr",label="Download table"), dataTableOutput("contr")),
-            tabPanel("Comparison with other cancers", downloadButton("download_known",label="Download table"), dataTableOutput("known"))
-            
-            
-#            tabPanel("proba",verbatimTextOutput("proba"))
-            
-            
+         mainPanel(id="mainpanel",
+            tabsetPanel(type="tabs",
+               tabPanel("Mutational profile of provided sample/s", plotOutput("prof96")),
+               tabPanel("Cosmic mutational signatures contributions", downloadButton("download_contr",label="Download table"), dataTableOutput("contr")),
+               tabPanel("Comparison with other cancers", downloadButton("download_known",label="Download table"), dataTableOutput("known"))
+                               
             )
+         )
       )
-   )
+
+      
+   ),
+
+   bsTooltip("datatype","Choose the format of the input file")
 ))
