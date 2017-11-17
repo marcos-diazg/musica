b53c09c7de66a248da1921257eaf395ba8679e6f
Marcos <m.diaz.gay@gmail.com>
2017-11-17 13:46:07 +0100

    Update download buttons (tiff format removed for heatmaplys)

--- server.R
+++ server.R
@@ -389,7 +389,7 @@ shinyServer(function(input, output,session){
          
          plot_smp + geom_bar(stat="identity",fill="orangered2") + theme_minimal() + geom_text(aes(label=smp), size=5, position = position_stack(vjust = 0.5), colour="white") + coord_flip() + labs(x = "", y = "Somatic mutation prevalence (number of mutations per megabase)") + theme(axis.text=element_text(size=12), axis.title = element_text(size = 13, face = "bold"), panel.grid.major.y=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank())       
          
-         ggsave(ff,height=7,width=7,dpi=ppi)
+         ggsave(ff,height=2*ncol(mutation_counts_new),width=10,dpi=ppi)
          
       }
    )
@@ -551,7 +551,7 @@ shinyServer(function(input, output,session){
           
           
           
-          ggsave(ff,height=7,width=7,dpi=ppi)
+          ggsave(ff,height=6,width=10,dpi=ppi)
        }
     )
     
