05db96778c3e63a16fde0f8e1d0893a2bb0cf23a
Maria Vila Casadesús <maria.vila.cs@gmail.com>
2018-04-24 14:14:32 +0200

    Correlation distance
    Heatmap of known signatures now running with correlation matrix

--- server.R
+++ server.R
@@ -717,7 +717,7 @@ shinyServer(function(input, output,session){
       
       heatmaply(a, scale_fill_gradient_fun = scale_fill_gradientn(colours = colorends, limits = c(0,3)),
                 dendrogram = dendro, k_row = 1, k_col = 1, column_text_angle = 90, hide_colorbar = TRUE,
-                hclustfun =  function(x) as.dist(1 - cor(t(x), method="pearson")) )
+                distfun = 'pearson')
       
    })
    
@@ -756,7 +756,7 @@ shinyServer(function(input, output,session){
            
            heatmaply(a, scale_fill_gradient_fun = scale_fill_gradientn(colours = colorends, limits = c(0,3)),
                      dendrogram = dendro, k_row = 1, k_col = 1, column_text_angle = 90, hide_colorbar = TRUE,
-                     hclustfun =  function(x) as.dist(1 - cor(t(x), method="pearson")),
+                     distfun = 'pearson',
                      file = ff)
         
       })
